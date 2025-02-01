141e20df90e8d82f2667a8a2f8114875144337a2
pupungeggang6 <pupungeggang6@gmail.com>
2025-02-01 19:14:18 +0900

    Temp

---
+++ platformer/Script/game.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+var key = {
+    'left': false,
+    'right': false,
+}
+
+func _ready():
+    pass
+    
+func _process(delta):
+    pass
--- platformer/Script/title.gd
+++ platformer/Script/title.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 func _ready():
-	pass
-	
+    pass
+
 func _process(delta):
-	pass
+    if Input.is_action_just_released("mouse"):
+        var scene = load("res://Scene/game.tscn").instantiate()
+        queue_free()
+        get_tree().root.add_child(scene)
